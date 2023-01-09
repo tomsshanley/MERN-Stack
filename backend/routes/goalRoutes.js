c6600b6ea3971b576ddd71592327391101fe18a1
Thomas Shanley <102588210@student.swin.edu.au>
2023-01-09 18:29:09 +1100

    adding protection to goal routes

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -3,23 +3,23 @@ const router = express.Router()
 
 //bring in some functions from the controllers to set as endpoints
 const {getGoals, setGoals, updateGoals, deleteGoals} = require('../controllers/goalContoller')
-
+const {protect} = require('../middleware/authMiddleware')
 
 
 //example get request
-router.get('/', getGoals)
+router.get('/', protect, getGoals)
 
 //post request
-router.post('/', setGoals)
+router.post('/', protect, setGoals)
 
 //the two above can be shortend to
 // router.route('/').get(getGoals).post(setGoals)
 
 //update request requires id
-router.put('/:id', updateGoals)
+router.put('/:id', protect, updateGoals)
 
 //delete reuqest
-router.delete('/:id', deleteGoals)
+router.delete('/:id', protect, deleteGoals)
 
 // the two above can be shortend to
 // router.route('/:id').put(updateGoals).delete(deleteGoals)
